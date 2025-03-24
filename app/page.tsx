1071375234244f0c823e021301736d23a6fa619d
olyaiy <alexolyaiy@gmail.com>
2025-03-24 12:47:01 -0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -39,19 +39,19 @@ const features = [
     step: 'For Creators', 
     title: 'For Creators',
     content: 'Build specialized AI agents and monetize your expertise.', 
-    image: 'https://images.unsplash.com/photo-1723958929247-ef054b525153?q=80&w=2070&auto=format&fit=crop' 
+    image: '/marketing/step-1.gif'
   },
   { 
     step: 'For Users',
     title: 'For Users',
     content: 'Discover and access tailored AI solutions for your specific needs',
-    image: 'https://images.unsplash.com/photo-1723931464622-b7df7c71e380?q=80&w=2070&auto=format&fit=crop'
+    image: '/marketing/step-2.png'
   },
   { 
     step: 'For Everyone',
     title: 'For Everyone',
     content: 'Enjoy a world where everyone can prosper from AI.',
-    image: 'https://images.unsplash.com/photo-1725961476494-efa87ae3106a?q=80&w=2070&auto=format&fit=crop'
+    image: '/marketing/step-3.png'
   },
 ]
 
