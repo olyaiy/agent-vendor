1168da26fde5f97e7cd9975601e55245c7d4f20d
olyaiy <alexolyaiy@gmail.com>
2025-04-07 15:35:47 -0700

    add ai input with search

---
+++ app/c/page.tsx
@@ -0,0 +1,10 @@
+import { AIInputWithSearch } from "@/components/ui/ai-input-with-search";
+
+export default function Page() {
+    return (
+        <div>
+
+            <AIInputWithSearch />
+        </div>
+    );
+}
