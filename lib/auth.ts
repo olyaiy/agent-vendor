ab7ef30c06a2cb0f28c7eea30361d9296eb235ab
olyaiy <alexolyaiy@gmail.com>
2025-04-08 11:54:24 -0700

    fi google

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,11 +2,13 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 import { anonymous, username } from "better-auth/plugins";
+import * as schema from "@/db/schema"; // <-- Import the schema index
 export const auth = betterAuth({
   
   // database
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema: schema.auth // <-- Pass the auth schema directly
   }),
 
   // social providers
