bc488f22e2a82c93576218843c6bda323f53879a
olyaiy <alexolyaiy@gmail.com>
2025-04-11 16:38:57 -0700

    removed

--- app/api/polar/checkout/route.ts
+++ app/api/polar/checkout/route.ts
@@ -4,5 +4,5 @@ import { Checkout } from '@polar-sh/nextjs'
 export const GET = Checkout({
   accessToken: process.env.POLAR_ACCESS_TOKEN!,
   successUrl: `${process.env.BETTER_AUTH_URL}/confirmation`,
-  server: 'sandbox', // Use this option if you're using the sandbox environment - else use 'production' or omit the parameter
+  // server: 'sandbox', // Use this option if you're using the sandbox environment - else use 'production' or omit the parameter
 });
