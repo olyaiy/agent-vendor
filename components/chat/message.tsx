8e46843b66b6e9b4e98e5e799e33c18705a00ef5
olyaiy <alexolyaiy@gmail.com>
2025-04-11 11:05:50 -0700

    fix user message alignment

--- components/chat/message.tsx
+++ components/chat/message.tsx
@@ -46,10 +46,10 @@ const PurePreviewMessage = ({
     >
       <div
         className={cn(
-          'flex gap-4 w-full group-data-[role=user]/message:ml-auto group-data-[role=user]/message:max-w-2xl',
+          'flex gap-4 w-full group-data-[role=user]/message:ml-auto group-data-[role=user]/message:max-w-2xl  ',
           {
             'w-full': mode === 'edit',
-            'group-data-[role=user]/message:w-fit': mode !== 'edit',
+            'group-data-[role=user]/message:w-fit ': mode !== 'edit',
           },
         )}
       >
@@ -63,7 +63,7 @@ const PurePreviewMessage = ({
           </div>
         )} */}
 
-        <div className="flex flex-col gap-4 w-full">
+        <div className="flex flex-col gap-4 w-full items-end">
           {message.experimental_attachments && (
             <div
               data-testid={`message-attachments`}
